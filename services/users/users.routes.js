171e754976276bb84f4aec702a9eae748f9c4799
Burhanuddin Rampura <burhanuddin.devstree@gmail.com>
2024-09-03 14:38:04 +0530

    added the missing functionality

--- services/users/users.routes.js
+++ services/users/users.routes.js
@@ -1,93 +1,74 @@
 const router = require("express").Router();
 const controller = require("./users.controller");
-const { guard } = require('../../helper');
+const { guard } = require("../../helper");
 const multerSetting = require("../../helper/multer").userImageUpload;
-const  {usersValidation} = require('./users.validation');
-
+const { usersValidation } = require("./users.validation");
 
 /*
  *  Register New User
  */
-router.post(
-    "/register",
-    controller.register
-);
+router.post("/register", controller.register);
 
 /*
  *  Login
  */
-router.post(
-    "/login",
-    controller.login
-);
+router.post("/login", controller.login);
 
 /*
  *  Resend verification Link
  */
-router.post(
-    "/resend-verification-link",
-    controller.resendVerificationLink
-);
+router.post("/resend-verification-link", controller.resendVerificationLink);
 
 /*
  *  Verify User Account
  */
-router.post(
-    "/verify-user",
-    controller.verifyUser
-);
+router.post("/verify-user", controller.verifyUser);
 
 /*
  *  Forgot Passowrd
  */
-router.post(
-    "/forgot-password",
-    controller.forgotPassword
-);
+router.post("/forgot-password", controller.forgotPassword);
 
 /*
  *  Reset Passowrd
  */
-router.post(
-    "/reset-password",
-    controller.resetPassword
-);
+router.post("/reset-password", controller.resetPassword);
 
 /*
  *  Update Profile
  */
 router.post(
-    "/update",
-    multerSetting,
-    guard.isAuthorized(['admin', 'organizer', 'player']),
-    controller.update
+  "/update",
+  multerSetting,
+  guard.isAuthorized(["admin", "organizer", "player"]),
+  controller.update
 );
-  
+
 /*
  *  Change Password
  */
 router.post(
-    "/change-password",
-    guard.isAuthorized(['admin', 'organizer', 'player']),
-    controller.changePassword
+  "/change-password",
+  guard.isAuthorized(["admin", "organizer", "player"]),
+  controller.changePassword
 );
 
 /*
  *  Get Profile
  */
 router.get(
-    "/get-profile",
-    guard.isAuthorized(['admin', 'organizer', 'player']),
-    controller.get
+  "/get-profile",
+  guard.isAuthorized(["admin", "organizer", "player"]),
+  controller.get
 );
 
 /*
  *  logout
  */
 router.post(
-    "/logout",
-    guard.isAuthorized(['admin', 'organizer', 'player']),
-    controller.logout
+  "/logout",
+  guard.isAuthorized(["admin", "organizer", "player"]),
+  controller.logout
 );
 
 module.exports = router;
--- services/users/users.services.js
+++ services/users/users.services.js
@@ -1,70 +1,76 @@
 const { commonResponse } = require("../../helper");
 const UsersModel = require("./users.model");
-const otpGenerator = require('otp-generator');
+const otpGenerator = require("otp-generator");
 
 /*
-*  Check Email Exist
-*/
+ *  Check Email Exist
+ */
 exports.is_exist = async (reqBody) => {
-    try {
-        let user = await UsersModel.findOne({email: reqBody.email}).lean();
-        if (!user) {
-          return false;
-        }
-        return user;
-    } catch (error) {
-        console.log("Error : ", error);
-        return new Error(error);
+  try {
+    let user = await UsersModel.findOne({ email: reqBody.email }).lean();
+    if (!user) {
+      return false;
     }
+    return user;
+  } catch (error) {
+    console.log("Error : ", error);
+    return new Error(error);
+  }
 };
 
 /*
-*  Get By Id
-*/
+ *  Get By Id
+ */
 exports.get = async (id) => {
-    try {
-        let user = await UsersModel.findOne({ _id: id }).lean();
-        if (!user) {
-            throw new Error(commonResponse.getErrorMessage("USER_NOT_FOUND"));
-        }
-        return user;
-    } catch (error) {
-        console.log("Error : ", error);
-        return new Error(error);
+  try {
+    let user = await UsersModel.findOne({ _id: id }).lean();
+    if (!user) {
+      throw new Error(commonResponse.getErrorMessage("USER_NOT_FOUND"));
     }
+    return user;
+  } catch (error) {
+    console.log("Error : ", error);
+    return new Error(error);
+  }
 };
 
 /*
-*  Add New User
-*/
+ *  Add New User
+ */
 exports.save = async (reqbody) => {
-    try {
-        const newUser = new UsersModel(reqbody);
-        return await newUser.save();
-    } catch (error) {
-        console.log("Error : ", error);
-        return new Error(error);
-    }
+  try {
+    const newUser = new UsersModel(reqbody);
+    return await newUser.save();
+  } catch (error) {
+    console.log("Error : ", error);
+    return new Error(error);
+  }
 };
 
 /*
-*  Update User
-*/
+ *  Update User
+ */
 exports.update = async (id, reqBody) => {
-    try {
-        let updateUserData = await UsersModel.findOneAndUpdate({ _id: id }, {$set:reqBody}, {new: true,}).lean();
-        return updateUserData;
-    } catch (error) {
-        console.log("Error : ", error);
-        return new Error(error);
-    }
+  try {
+    let updateUserData = await UsersModel.findOneAndUpdate(
+      { _id: id },
+      { $set: reqBody },
+      { new: true }
+    ).lean();
+    return updateUserData;
+  } catch (error) {
+    console.log("Error : ", error);
+    return new Error(error);
+  }
 };
 
 /*
-*  Otp Generator
-*/
+ *  Otp Generator
+ */
 
-exports.otpGenerate = async() => {
-    return await otpGenerator.generate(6, { upperCase: false, specialChars: false });
-   
-}
+exports.otpGenerate = async () => {
+  return await otpGenerator.generate(6, {
+    upperCase: false,
+    specialChars: false,
+  });
+};
